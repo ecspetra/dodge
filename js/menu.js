4fa8ff965846b56b3240316eab4c0e0988f67bca
ecspetra <ecspetra@gmail.com>
2021-05-27 00:01:17 +0300

    fix

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,14 @@ $(window).scroll(function () {
     $(".header-container").removeClass("lightHeader");
   }
 });
+$(document).ready(function () {
+  $(".header__content__navigation a").click(function (event) {
+    $(".header__content__burger, .header__content__navigation").toggleClass(
+      "active-burger"
+    );
+    $("body").toggleClass("lock");
+  });
+});
 
 $(document).ready(function () {
   $(".header__content__burger").click(function (event) {
